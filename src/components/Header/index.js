eb089e0bf0e099e7ae27af10ff9f9e2b043a6544
Kyle Treptow <kyletreptow@kyles-mbp.gateway.pace.com>
2017-05-23 23:39:50 -0700

    First styling edits, SASS implemented

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+    render() {
+    return (
+      <header id="app-header">
+        <div className="container-fluid">
+          <p>Header Here...</p>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,7 +1,8 @@
 // @flow
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 import ToDoList from '../../components/ToDoList';
 import ToDoForm from '../../components/ToDoForm';
 import {formSubmit} from '../../actions/app.js';
@@ -10,9 +11,11 @@ class Home extends Component {
   onSubmit = data => this.props.formSubmit(data); 
   render() {
     return (
-      <div className="container">
+      <div id="app-wrapper">
+        <Header />
+        <ToDoForm onSubmit={this.onSubmit} />
   		<ToDoList />
-  		<ToDoForm onSubmit={this.onSubmit} />
+  		<Footer />
       </div>
     );
   }
